2bbca3fb30ea877d23df638005702b6a10443aaa
Fahad Khan <fahadnaeemkhan@gmail.com>
2024-02-09 17:32:26 -0800

    v2: with common query structure

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/grafana/mimir/integration/e2emimir"
@@ -30,7 +31,15 @@ type TSDBQueryResult []TimeSeries
 
 // timestamp time.Time, timeout time.Duration,
 type TSDBStore interface {
-	Query(ctx context.Context, query string, opts map[string]any) TSDBQueryResult
+	Query(ctx context.Context, query TSQuery, opts map[string]any) TSDBQueryResult
+}
+
+type TSQuery struct {
+	Table     string            `json:"table"`
+	StartTime time.Time         `json:"startTime"`
+	EndTime   time.Time         `json:"endTime"`
+	Step      time.Duration     `json:"step"`
+	Filters   map[string]string `json:"filters"`
 }
 
 // *** Mimir ************************
@@ -48,17 +57,29 @@ func NewMimirDBStore(url string, id string) MimirDBStore {
 	return MimirDBStore{Client: mimirE2eClient}
 }
 
-func (r MimirDBStore) Query(ctx context.Context, query string, opts map[string]any) TSDBQueryResult {
-	// TODO: check errors
-	startTime := opts["start_time"].(time.Time)
-	endTime := opts["end_time"].(time.Time)
-	step := opts["step"].(time.Duration)
-
+/*
+	 Query String Example:
+		query := `bits{index_num="bb1-ngn.gv51.1001"}`
+*/
+func (r MimirDBStore) Query(ctx context.Context, query TSQuery, opts map[string]any) TSDBQueryResult {
 	if timeout, ok := opts["timeout"]; ok {
 		r.Client.SetTimeout(timeout.(time.Duration))
 	}
 
-	resp, err := r.Client.QueryRange(query, startTime, endTime, step)
+	// build query string
+	var queryBuilder strings.Builder
+	queryBuilder.WriteString(query.Table)
+	queryBuilder.WriteString("{")
+	for k, v := range query.Filters {
+		queryBuilder.WriteString(k)
+		queryBuilder.WriteString("=")
+		queryBuilder.WriteString("\"")
+		queryBuilder.WriteString(v)
+		queryBuilder.WriteString("\"")
+	}
+	queryBuilder.WriteString("}")
+
+	resp, err := r.Client.QueryRange(queryBuilder.String(), query.StartTime, query.EndTime, query.Step)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -124,10 +145,31 @@ func NewInfluxDBStore(url string, token string) InfluxDBStore {
 	return InfluxDBStore{Client: influxClient}
 }
 
-func (r InfluxDBStore) Query(ctx context.Context, query string, opts map[string]any) TSDBQueryResult {
+/*
+	 Query String Example:
+		query := `from(bucket: "testing_script")
+		|> range( start: 2024-02-05, stop: 2024-02-09)
+		|> filter(fn: (r) => r["_field"] == "bits")
+		|> filter(fn: (r) => r["index_num"] == "bb1-ngn.gv51.1001")
+		|> aggregateWindow(every: 5m, fn: last, createEmpty: false)
+		`
+*/
+func (r InfluxDBStore) Query(ctx context.Context, query TSQuery, opts map[string]any) TSDBQueryResult {
+	// TODO: both bucket and org should moved to config file
 	queryAPI := r.Client.QueryAPI(opts["org"].(string))
+	bucket := opts["bucket"].(string)
 
-	result, err := queryAPI.Query(ctx, query)
+	// build query string
+	var queryBuilder strings.Builder
+	queryBuilder.WriteString(fmt.Sprintf("from(bucket: %q)\n", bucket))
+	queryBuilder.WriteString(fmt.Sprintf("|> range( start: %s, stop: %s)\n", query.StartTime.Format(time.RFC3339), query.EndTime.Format(time.RFC3339)))
+	queryBuilder.WriteString(fmt.Sprintf("|> filter(fn: (r) => r[\"_field\"] == %q)\n", query.Table))
+	for k, v := range query.Filters {
+		queryBuilder.WriteString(fmt.Sprintf("|> filter(fn: (r) => r[%q] == %q)\n", k, v))
+	}
+	queryBuilder.WriteString(fmt.Sprintf("|> aggregateWindow(every: %s, fn: last, createEmpty: false)", query.Step))
+
+	result, err := queryAPI.Query(ctx, queryBuilder.String())
 
 	if err != nil {
 		log.Fatalln(err)
@@ -160,7 +202,7 @@ func (r InfluxDBStore) Query(ctx context.Context, query string, opts map[string]
 			Labels:          labels,
 			TimeValueSeries: []TimeValue{tv},
 		})
-		//fmt.Printf("value: %v, measurement: %v, field: %v, time: %v\n", result.Record().Values(), result.Record().Measurement(), result.Record().Field(), result.Record().Time())
+		//gfmt.Printf("value: %v, measurement: %v, field: %v, time: %v\n", result.Record().Values(), result.Record().Measurement(), result.Record().Field(), result.Record().Time())
 	}
 	// check for an error
 	if result.Err() != nil {
@@ -171,19 +213,23 @@ func (r InfluxDBStore) Query(ctx context.Context, query string, opts map[string]
 }
 
 func main() {
+	location, err := time.LoadLocation("UTC")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := TSQuery{
+		Table:     "bits",
+		StartTime: time.Date(2024, time.February, 5, 0, 0, 0, 0, location),
+		EndTime:   time.Date(2024, time.February, 9, 0, 0, 0, 0, location),
+		Filters:   map[string]string{"index_num": "bb1-ngn.gv51.1001"},
+		Step:      time.Minute * 30,
+	}
+
 	// var tsdbStore1 TSDBStore = NewMimirDBStore("sv5-edn-mimir-stg.lab.equinix.com", "eot-telemetry")
-	// pretty.Print(tsdbStore1.Query(context.Background(), `bits{index_num="bb1-ngn.gv51.1001"}`, map[string]any{
-	// 	"start_time": time.Now().Add(3 * 24 * -time.Hour),
-	// 	"end_time":   time.Now().Add(2 * 24 * -time.Hour),
-	// 	"step":       time.Minute * 30,
-	// }))
+	// pretty.Print(tsdbStore1.Query(context.Background(), query, map[string]any{}))
 
 	var tsdbStore2 TSDBStore = NewInfluxDBStore("http://devsv3ednmgmt09.lab.equinix.com:30320", "mytoken")
-
-	query := `from(bucket: "testing_script")
-	|> range( start: -3d, stop: -2d)
-	|> filter(fn: (r) => r["_field"] == "bits")
-	|> filter(fn: (r) => r["index_num"] == "bb1-ngn.gv51.1001")`
-
-	pretty.Print(tsdbStore2.Query(context.Background(), query, map[string]any{"org": "primary"}))
+	pretty.Print(tsdbStore2.Query(context.Background(), query, map[string]any{"org": "primary", "bucket": "testing_script"}))
 }
